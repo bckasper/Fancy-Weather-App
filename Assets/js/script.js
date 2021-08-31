cc1cc2190200b4d210d7f3381e8c962f7a3f2372
Ben Kasper <bckasper@gmail.com>
2021-08-31 15:54:22 -0400

    added a throw...catch if the user enters a city that does not return a 200 response. Also began to work through printing result objects to the DOM when the search bar is submitted

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,6 +2,10 @@
 const cityInput = document.getElementById('city-input')
 const searchFormEl = document.getElementById('search-form')
 const city = document.getElementById('city-input')
+const currentCityEl = document.getElementById('current-city')
+const currentCityIconEl = document.getElementById('current-city-icon')
+const currentCityTemp = document.getElementById('current-weather-temp')
+
 
 // Global variables
 var units = 'imperial'
@@ -11,26 +15,34 @@ var APIKey = '35610e0fadafaec7131373302bc9e1ab'
 
 // API request to get the current weather for the city the user inputs
 function getCurrentWeather(){
-    currentQueryURL = 'https://api.openweathermap.org/data/2.5/weather?q='+city.value.trim()+',&appid='+APIKey+'&units='+units
+    currentQueryURL = 'https://api.openweathermap.org/data/2.5/weather?q='+city.value.trim()+'&appid='+APIKey+'&units='+units
 
     // console.log(currentQueryURL)
 
     fetch(currentQueryURL)
         .then(function(response){
+            if(!response.ok){
+                throw response.json()
+            }
+           
             return response.json()
         })
         .then(function(result){
             console.log(result)
+            printCurrentWeather(result)
+        })
+        .catch(function(){
+            alert('City not found. Please try again!')
         })
 }
 
-// API request to get the 5-day forcase for the city the user inputs
+// API request to get the 5-day forcast for the city the user inputs
 function getForecast(){
-    forecastQueryURL = 'https://api.openweathermap.org/data/2.5/forecast?q='+city.value.trim()+',&appid='+APIKey+'&units='+units+'&cnt=5'
+    forecastQueryURL = 'https://api.openweathermap.org/data/2.5/forecast?q='+city.value.trim()+'&appid='+APIKey+'&units='+units+'&cnt=5'
 
     fetch(forecastQueryURL)
         .then(function(response){
-            return response.json()
+            return response.json() 
         })
         .then(function(result){
             console.log(result)
@@ -47,11 +59,21 @@ function storeInput(){
 }
 
 
+function printCurrentWeather(result){
+    
+    currentCityEl.innerText = result.name
+    currentCityTemp.innerHTML = Math.round(result.main.temp)+'° F'
+    
+}
+
+
 // Event listener that handles the user's form submission. Stores the input into local storage and then fires the getCurrentWeather and getForecast functions for API requests
 searchFormEl.addEventListener('submit', function(event){
     event.preventDefault()
 
     storeInput()
     getCurrentWeather()
-    getForecast()
+    // getForecast()
+
+    city.value = '';
 })
